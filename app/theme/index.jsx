c0e7c5b3aa89eeac709c10c7115b2ca797427e6e
gustavost26 <gteles@trivalor.pt>
2025-05-02 20:07:22 +0100

    feat: Page theme

---
+++ app/theme/index.jsx
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Theme() {
+  return (
+    <View>
+      <Text>Theme</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+
+});
