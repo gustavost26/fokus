41c8247763cc1cde7dee2792b851e39be8dc02d0
gustavost26 <gteles@trivalor.pt>
2025-05-05 01:14:22 +0100

    feat: Theme change styling file.

---
+++ constants/Colors.js
@@ -0,0 +1,69 @@
+export const Colors = {
+  primary: "#6849a7",
+  secondary: "#E4E4E4",
+  white: "#FFFFFF",
+  black: "#000000",
+  warning: "#cc475a",
+
+  light: '#FBFBFB',
+  dark: '#0c263b',
+  btnLight: '#F6F5F2',
+  btnDark: '#09122C',
+  btnRight: '#7743DB',
+  black: '#171617',
+  white: '#fefeff',
+  gray: '#f2f2f4',
+
+  //3b413f
+  //3E3E3E
+  dark: {
+    text: "#fff",
+    title: "#fff",
+    backgroundPrimary: "#212427",
+    backgroundSecondary: "#3E3E3E",
+    backgroundInput: "#212427",
+    action: {
+      background: "#3b413f80",
+      border: "#3b413f",
+      active: "#212427"
+    },
+    cardCompleted: "#0F725C",
+    footerText: "#98A0A8",
+    footerTextDeveloper: "#FFF",
+    cardTheme: "#3E3E3E"
+  },
+
+  light: {
+    text: "#625f72",
+    title: "#201e2b",
+    backgroundPrimary: "#F6F6F6", //f2f2f4
+    backgroundSecondary: "#CFCFCF",
+    backgroundInput: "#F6F6F6",
+    action: {
+      background: "#CFCFCF80",
+      border: "#CFCFCF",
+      active: "#CFCFCF"
+    },
+    cardCompleted: "#00a878",
+    footerText: "#625f72",
+    footerTextDeveloper: "#000",
+    cardTheme: "#CFCFCF80"
+  },
+
+  default: {
+    text: "#FFF",
+    title: "#FFF",
+    backgroundPrimary: "#021123",
+    backgroundSecondary: "#98A0A8",
+    backgroundInput: "#FFF",
+    action: {
+      background: "#14448080",
+      border: "#144480",
+      active: "#144480"
+    },
+    cardCompleted: "#0F725C",
+    footerText: "#98A0A8",
+    footerTextDeveloper: "#FFF",
+    cardTheme: "#14448080"
+  }
+}
